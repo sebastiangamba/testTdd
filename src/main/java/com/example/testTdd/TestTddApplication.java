f91e8b2a185c77c7be327e557a527f7985afb147
Sebastian Gamba <sebastian.gamba@avaldigitallabs.com>
2019-05-08 11:49:40 -0500

    Commit inicial con dependencias de testing

---
+++ src/main/java/com/example/testTdd/TestTddApplication.java
@@ -0,0 +1,13 @@
+package com.example.testTdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTddApplication.class, args);
+	}
+
+}
